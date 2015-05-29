53c36f418077fe0b787c93d749998bee5cecccef
paul <pmorin35@gmail.com>
2015-05-29 09:54:11 -0700

    pass first rspec. replace vowel with a dash

--- lib/word_puzzle.rb
+++ lib/word_puzzle.rb
@@ -1,5 +1,16 @@
 class String
   define_method(:word_puzzle) do
-    #code here
+    vowels = ["a", "e", "i", "o", "u"]
+    old_letters = self.split("")
+    new_letters = []
+    old_letters.each() do |letter|
+      if vowels.include?(letter)
+        new_letter = "-"
+      else
+        new_letter = letter
+      end
+      new_letters.push(new_letter)
+    end
+    new_letters.join()
   end
 end
